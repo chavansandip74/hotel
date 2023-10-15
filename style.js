eb86cc5565fbe9b45679a00999578f4f29eff2b3
sandip chavan <121749116+chavansandip74@users.noreply.github.com>
2023-10-15 16:58:08 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,30 @@
+function dark() {
+    document.getElementById('body').style.background = "rgba(0, 0, 0,0.8)";
+   documwnt.getElementById('tx').style.color="white";
+   document.getElementsByClassName('txc').style.color="red";
+}
+function upit()
+{
+    window.scrollTo(0,0);
+}
+function info(){
+            
+    window.scrollTo(0, 400);
+}
+function con(){
+            
+    window.scrollTo(0, 2900);
+}
+function about(){
+    
+    window.scrollTo(0, 1500);
+}
+function test(){
+    
+    window.scrollTo(0, 2500);
+}
+
+function gall(){
+    
+    window.scrollTo(0, 2080);
+}
